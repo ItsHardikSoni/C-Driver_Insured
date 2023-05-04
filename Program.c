61b29818ea4b5506fd7dfcec1070c16e3812a451
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-04 12:42:32 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -1,6 +1,6 @@
 // A company insures its drivers in the following cases: 
-// If the driver is married. If the driver is unmarried, male &amp; above 30 years of age. 
-// If the driver is unmarried, female &amp; above 25 years of age.
+// If the driver is married. If the driver is unmarried, male & above 30 years of age. 
+// If the driver is unmarried, female & above 25 years of age.
 // They can be combined together using && and ||
 
 
